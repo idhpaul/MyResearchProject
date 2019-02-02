53376374606275b86a1dfd4167d5fa50be847b4b
DongHyun-IMM <idhpaul@naver.com>
2019-02-02 21:10:30 +0900

    First Init

---
+++ RnD_LOGnDebugFunction/RnD_LOGnDebugFunction/src/log_test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <libgen.h>
+#include <log_util.h>
+
+int my_test_test_func(int value)
+{
+    LOG_TRACE("TRACE LOG...1");
+    LOG_DEBUG("TEST");
+	printf("%d\n", value);
+    LOG_TRACE("TRACE LOG...2");
+    LOG_DEBUG("%s", "sample test");
+}
+
+int main(int argc, char **argv)
+{
+	int ret;
+	int value = 0;
+
+    LOGsetInfo(".", basename(argv[0]));
+
+    LOG_TRACE("TRACE LOG...1");
+    LOG_DEBUG("TEST");
+    LOG_TRACE("TRACE LOG...2");
+    LOG_CALL(ret = my_test_test_func(value));
+    LOG_DEBUG("%s", "sample test");
+    LOG_TRACE("TRACE LOG...3");
+    LOG_ERROR("%s 오류입니다.", "sample test");
+    LOG_TRACE("TRACE LOG...4");
+    return 0;
+}
