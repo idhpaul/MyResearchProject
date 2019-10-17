d9aa095fe13961c49695bf8d33f7df885173148b
IDH <idhpaul@naver.com>
2019-10-17 17:26:20 +0900

    add tls test code

--- RnD_Boost Projects/TLS_SYNC/RnD_Boost_TLS_sync/RnD_TLS_sync_client/main_tls_client.cpp
+++ RnD_Boost Projects/TLS_SYNC/RnD_Boost_TLS_sync/RnD_TLS_sync_client/main_tls_client.cpp
@@ -1,144 +1,319 @@
-// You have to .pem(인증서) file
+ #include <boost/bind.hpp>
+ #include <boost/asio.hpp>
+ #include <boost/asio/ssl.hpp>
+ #include <iostream>
 
-#include <boost/asio.hpp>
-#include <boost/asio/ssl.hpp>
-#include <iostream>
+ using namespace boost;
 
-using namespace boost;
+ class SyncSSLClient {
+ public:
+ 	SyncSSLClient(const std::string& raw_ip_address,
+ 		unsigned short port_num) :
+ 		m_ep(asio::ip::address::from_string(raw_ip_address),port_num),
+ 		m_ssl_context(asio::ssl::context::sslv3),
+ 		m_ssl_stream(m_ios, m_ssl_context)
+ 	{
+         // Set ssl context verification file    
+         m_ssl_context.load_verify_file("user.pem");
 
-class SyncSSLClient {
-public:
-	SyncSSLClient(const std::string& raw_ip_address,
-		unsigned short port_num) :
-		m_ep(asio::ip::address::from_string(raw_ip_address),
-			port_num),
-		m_ssl_context(asio::ssl::context::tls_client),
-		m_ssl_stream(m_ios, m_ssl_context)
-	{
-		// Set ssl context verification file    
-		m_ssl_context.load_verify_file("./user.pem");
+ 		std::cout << "sslclient load_verify_file ok" << std::endl;
 
-		// Set verification mode and designate that 
-		// we want to perform verification.
-		m_ssl_stream.set_verify_mode(asio::ssl::verify_peer);
+ 		// Set verification mode and designate that 
+ 		// we want to perform verification.
+ 		m_ssl_stream.set_verify_mode(asio::ssl::verify_peer);
 
-		// Set verification callback. 
-		m_ssl_stream.set_verify_callback([this](
-			bool preverified,
-			asio::ssl::verify_context& context)->bool {
-			return on_peer_verify(preverified, context);
-		});
-	}
-
-	void connect() {
-		// Connect the TCP socket.
-		m_ssl_stream.lowest_layer().connect(m_ep);
-
-		// Perform the SSL handshake.
-		m_ssl_stream.handshake(asio::ssl::stream_base::client);
-	}
-
-	void close() {
-		// We ignore any errors that might occur
-		// during shutdown as we anyway can't
-		// do anything about them.
-		boost::system::error_code ec;
-
-		m_ssl_stream.shutdown(ec); // Shutown SSL.
-
-		// Shut down the socket.
-		m_ssl_stream.lowest_layer().shutdown(
-			boost::asio::ip::tcp::socket::shutdown_both, ec);
-
-		m_ssl_stream.lowest_layer().close(ec);
-	}
-
-	std::string emulate_long_computation_op(
-		unsigned int duration_sec) {
-
-		// std::string request = "EMULATE_LONG_COMP_OP "
-		// 	+ std::to_string(duration_sec)
-		// 	+ "\n";
-
-		// send_request(request);
-		return receive_response();
-	};
-
-private:
-	bool on_peer_verify(bool preverified,
-		asio::ssl::verify_context& context)
-	{
-		// Here the certificate should be verified and the
-		// verification result should be returned.
-		char subject_name[256];
-		X509* cert = X509_STORE_CTX_get_current_cert(context.native_handle());
-		X509_NAME_oneline(X509_get_subject_name(cert), subject_name, 256);
-		std::cout << "Verifying " << subject_name << "\n";
-
-		return preverified;
-
-	}
-
-	void send_request(const std::string& request) {
-		asio::write(m_ssl_stream, asio::buffer(request));
-	}
-
-	std::string receive_response() {
-		asio::streambuf buf;
-		asio::read_until(m_ssl_stream, buf, '\n');
-
-		std::string response;
-		std::istream input(&buf);
-		std::getline(input, response);
-
-		return response;
-	}
-
-private:
-	asio::io_service m_ios;
-	asio::ip::tcp::endpoint m_ep;
-
-	asio::ssl::context m_ssl_context;
-	asio::ssl::stream<asio::ip::tcp::socket> m_ssl_stream;
-};
-
-int main()
-{
-	const std::string raw_ip_address = "192.168.81.1";
-	const unsigned short port_num = 9090;
-
-	int i = 0;
-
-	try {
-		SyncSSLClient client(raw_ip_address, port_num);
-
-		// Sync connect.
-		client.connect();
-
-		while (true)
-		{
-			if (i > 10)
-			{
-				break;
-			}
-
-			std::string response = client.emulate_long_computation_op(10);
-
-			std::cout << "Response received: " << response << std::endl;
-
-			i++;
-		}
+ 		std::cout << "sslclient set_verify_mode ok" << std::endl;
 
 
-		// Close the connection and free resources.
-		client.close();
-	}
-	catch (system::system_error &e) {
-		std::cout << "Error occured! Error code = " << e.code()
-			<< ". Message: " << e.what();
+ 		// Set verification callback. 
+         m_ssl_stream.set_verify_callback([this](
+ 			bool preverified,
+ 			asio::ssl::verify_context& context)->bool{
+ 			return on_peer_verify(preverified, context);
+ 		});
 
-		return e.code().value();
-	}
+ 		m_ssl_stream.set_verify_callback(
+ 			boost::bind(&SyncSSLClient::on_peer_verify, this, _1, _2));
 
-	return 0;
-}
+ 		std::cout << "sslclient set_verify_callback ok" << std::endl;
+
+ 	}
+
+ 	void connect() {
+ 		// Connect the TCP socket.
+ 		m_ssl_stream.lowest_layer().connect(m_ep);
+
+ 		std::cout << "sslclient connect ok" << std::endl;
+
+
+ 		// Perform the SSL handshake.
+ 		m_ssl_stream.handshake(asio::ssl::stream_base::client);
+
+ 		std::cout << "sslclient handshake ok" << std::endl;
+
+ 	}
+
+ 	void close() {
+ 		// We ignore any errors that might occur
+ 		// during shutdown as we anyway can't
+ 		// do anything about them.
+ 		boost::system::error_code ec;
+
+ 		m_ssl_stream.shutdown(ec); // Shutown SSL.
+
+ 		// Shut down the socket.
+ 		m_ssl_stream.lowest_layer().shutdown(
+ 			boost::asio::ip::tcp::socket::shutdown_both, ec);
+
+ 		m_ssl_stream.lowest_layer().close(ec);
+ 	}
+
+ 	std::string emulate_long_computation_op(
+ 		unsigned int duration_sec) {
+
+ 		// std::string request = "EMULATE_LONG_COMP_OP "
+ 		// 	+ std::to_string(duration_sec)
+ 		// 	+ "\n";
+
+ 		// send_request(request);
+ 		return receive_response();
+ 	};
+
+ private:
+ 	bool on_peer_verify(bool preverified,
+ 		asio::ssl::verify_context& context)
+ 	{
+ 		// Here the certificate should be verified and the
+ 		// verification result should be returned.
+         char subject_name[256];
+ 		X509* cert = X509_STORE_CTX_get_current_cert(context.native_handle());
+ 		X509_NAME_oneline(X509_get_subject_name(cert), subject_name, 256);
+ 		std::cout << "Verifying " << subject_name << "\n";
+
+ 		return preverified;
+
+ 	}
+
+ 	void send_request(const std::string& request) {
+ 		asio::write(m_ssl_stream, asio::buffer(request));
+ 	}
+
+ 	std::string receive_response() {
+ 		asio::streambuf buf;
+ 		asio::read_until(m_ssl_stream, buf, '\n');
+
+ 		std::string response;
+ 		std::istream input(&buf);
+ 		std::getline(input, response);
+
+ 		return response;
+ 	}
+
+ private:
+ 	asio::io_service m_ios;
+ 	asio::ip::tcp::endpoint m_ep;
+
+ 	asio::ssl::context m_ssl_context;
+ 	asio::ssl::stream<asio::ip::tcp::socket> m_ssl_stream;
+ };
+
+ int main()
+ {
+
+ 	std::string input_str;
+
+ 	//std::cin >> input_str;
+
+ 	//std::cout << "User Input Str : " << input_str << std::endl;
+
+ 	const std::string raw_ip_address = "192.168.81.1";
+ 	const unsigned short port_num = 9090;
+
+     int i=0;
+
+ 	try {
+ 		SyncSSLClient client(raw_ip_address, port_num);
+
+ 		std::cout << "sslclient class ok" << std::endl;
+
+
+ 		// Sync connect.
+ 		client.connect();
+
+ 		std::cout << "client connect ok" << std::endl;
+
+
+         while(true)
+         {
+             if(i>10)
+             {
+                 break;
+             }
+
+             std::string response = client.emulate_long_computation_op(10);
+
+ 		    std::cout << "Response received: " << response << std::endl;
+
+             i++;
+         }
+
+
+ 		// Close the connection and free resources.
+ 		client.close();
+ 	}
+ 	catch (system::system_error &e) {
+ 		std::cout << "Error occured! Error code = " << e.code()
+ 			<< ". Message: " << e.what();
+
+ 		return e.code().value();
+ 	}
+
+ 	return 0;
+ }
+
+
+//This is Original SRC
+//// You have to .pem(인증서) file
+//
+//#include <boost/asio.hpp>
+//#include <boost/asio/ssl.hpp>
+//#include <iostream>
+//
+//using namespace boost;
+//
+//class SyncSSLClient {
+//public:
+//	SyncSSLClient(const std::string& raw_ip_address,
+//		unsigned short port_num) :
+//		m_ep(asio::ip::address::from_string(raw_ip_address),
+//			port_num),
+//		m_ssl_context(asio::ssl::context::tls_client),
+//		m_ssl_stream(m_ios, m_ssl_context)
+//	{
+//		// Set ssl context verification file    
+//		m_ssl_context.load_verify_file("./user.pem");
+//
+//		// Set verification mode and designate that 
+//		// we want to perform verification.
+//		m_ssl_stream.set_verify_mode(asio::ssl::verify_peer);
+//
+//		// Set verification callback. 
+//		m_ssl_stream.set_verify_callback([this](
+//			bool preverified,
+//			asio::ssl::verify_context& context)->bool {
+//			return on_peer_verify(preverified, context);
+//		});
+//	}
+//
+//	void connect() {
+//		// Connect the TCP socket.
+//		m_ssl_stream.lowest_layer().connect(m_ep);
+//
+//		// Perform the SSL handshake.
+//		m_ssl_stream.handshake(asio::ssl::stream_base::client);
+//	}
+//
+//	void close() {
+//		// We ignore any errors that might occur
+//		// during shutdown as we anyway can't
+//		// do anything about them.
+//		boost::system::error_code ec;
+//
+//		m_ssl_stream.shutdown(ec); // Shutown SSL.
+//
+//		// Shut down the socket.
+//		m_ssl_stream.lowest_layer().shutdown(
+//			boost::asio::ip::tcp::socket::shutdown_both, ec);
+//
+//		m_ssl_stream.lowest_layer().close(ec);
+//	}
+//
+//	std::string emulate_long_computation_op(
+//		unsigned int duration_sec) {
+//
+//		// std::string request = "EMULATE_LONG_COMP_OP "
+//		// 	+ std::to_string(duration_sec)
+//		// 	+ "\n";
+//
+//		// send_request(request);
+//		return receive_response();
+//	};
+//
+//private:
+//	bool on_peer_verify(bool preverified,
+//		asio::ssl::verify_context& context)
+//	{
+//		// Here the certificate should be verified and the
+//		// verification result should be returned.
+//		char subject_name[256];
+//		X509* cert = X509_STORE_CTX_get_current_cert(context.native_handle());
+//		X509_NAME_oneline(X509_get_subject_name(cert), subject_name, 256);
+//		std::cout << "Verifying " << subject_name << "\n";
+//
+//		return preverified;
+//
+//	}
+//
+//	void send_request(const std::string& request) {
+//		asio::write(m_ssl_stream, asio::buffer(request));
+//	}
+//
+//	std::string receive_response() {
+//		asio::streambuf buf;
+//		asio::read_until(m_ssl_stream, buf, '\n');
+//
+//		std::string response;
+//		std::istream input(&buf);
+//		std::getline(input, response);
+//
+//		return response;
+//	}
+//
+//private:
+//	asio::io_service m_ios;
+//	asio::ip::tcp::endpoint m_ep;
+//
+//	asio::ssl::context m_ssl_context;
+//	asio::ssl::stream<asio::ip::tcp::socket> m_ssl_stream;
+//};
+//
+//int main()
+//{
+//	const std::string raw_ip_address = "192.168.81.1";
+//	const unsigned short port_num = 9090;
+//
+//	int i = 0;
+//
+//	try {
+//		SyncSSLClient client(raw_ip_address, port_num);
+//
+//		// Sync connect.
+//		client.connect();
+//
+//		while (true)
+//		{
+//			if (i > 10)
+//			{
+//				break;
+//			}
+//
+//			std::string response = client.emulate_long_computation_op(10);
+//
+//			std::cout << "Response received: " << response << std::endl;
+//
+//			i++;
+//		}
+//
+//
+//		// Close the connection and free resources.
+//		client.close();
+//	}
+//	catch (system::system_error &e) {
+//		std::cout << "Error occured! Error code = " << e.code()
+//			<< ". Message: " << e.what();
+//
+//		return e.code().value();
+//	}
+//
+//	return 0;
+//}
--- RnD_Boost Projects/TLS_SYNC/RnD_Boost_TLS_sync/RnD_TLS_sync_server/main_tls_server.cpp
+++ RnD_Boost Projects/TLS_SYNC/RnD_Boost_TLS_sync/RnD_TLS_sync_server/main_tls_server.cpp
@@ -47,14 +47,14 @@ public:
 		m_ios(ios),
 		m_acceptor(m_ios,
 			asio::ip::tcp::endpoint(
-				asio::ip::address_v4::any(),
+				asio::ip::address_v4::from_string("192.168.81.1"),
 				port_num)),
-		m_tls_context(asio::ssl::context::tls_server)
+		m_tls_context(asio::ssl::context::sslv3)
 	{
 		// Setting up the context.
 		m_tls_context.set_options(
 			boost::asio::ssl::context::default_workarounds
-			| boost::asio::ssl::context::no_sslv2
+			//| boost::asio::ssl::context::no_sslv2
 			| boost::asio::ssl::context::single_dh_use);
 
 
@@ -209,3 +209,217 @@ int main()
 
 	return 0;
 }
+
+
+//This is Original SRC
+//#include <boost/asio.hpp>
+//#include <boost/asio/ssl.hpp>
+//
+//#include <thread>
+//#include <atomic>
+//#include <iostream>
+//
+//using namespace boost;
+//
+//class Service {
+//public:
+//	Service() {}
+//
+//	void handle_client(asio::ssl::stream<asio::ip::tcp::socket>& ssl_stream)
+//	{
+//		try {
+//			// Blocks until the handshake completes.
+//			ssl_stream.handshake(asio::ssl::stream_base::server);
+//
+//			asio::streambuf request;
+//			asio::read_until(ssl_stream, request, '\n');
+//
+//			// Emulate request processing.
+//			int i = 0;
+//			while (i != 1000000)
+//				i++;
+//			std::this_thread::sleep_for(
+//				std::chrono::milliseconds(500));
+//
+//			// Sending response.
+//			std::string response = "Response\n";
+//			asio::write(ssl_stream, asio::buffer(response));
+//
+//			std::cout << "Send Data to client" << std::endl;
+//		}
+//		catch (system::system_error &e) {
+//			std::cout << "Error occured! Error code = "
+//				<< e.code() << ". Message: "
+//				<< e.what();
+//		}
+//	}
+//};
+//
+//class Acceptor {
+//public:
+//	Acceptor(asio::io_service& ios, unsigned short port_num) :
+//		m_ios(ios),
+//		m_acceptor(m_ios,
+//			asio::ip::tcp::endpoint(
+//				asio::ip::address_v4::any(),
+//				port_num)),
+//		m_tls_context(asio::ssl::context::tls_server)
+//	{
+//		// Setting up the context.
+//		m_tls_context.set_options(
+//			boost::asio::ssl::context::default_workarounds
+//			| boost::asio::ssl::context::no_sslv2
+//			| boost::asio::ssl::context::single_dh_use);
+//
+//
+//		m_tls_context.use_certificate_chain_file("root.pem");
+//		m_tls_context.use_private_key_file("root.key", boost::asio::ssl::context::pem);
+//		m_tls_context.use_tmp_dh_file("dh2048.pem");
+//
+//		// Start listening for incoming connection requests.
+//		m_acceptor.listen();
+//	}
+//
+//	~Acceptor()
+//	{
+//		delete m_tls_stream;
+//	}
+//
+//	void accept(bool* connectstate) {
+//
+//		try {
+//			m_tls_stream = new asio::ssl::stream<asio::ip::tcp::socket>(m_ios, m_tls_context);
+//
+//			m_acceptor.accept(m_tls_stream->lowest_layer());
+//
+//			m_tls_stream->handshake(asio::ssl::stream_base::server);
+//
+//			// Accept and Handshaking ok
+//			*connectstate = true;
+//
+//		}
+//		catch (system::system_error &e) {
+//			std::cout << "Error occured! Error code = "
+//				<< e.code() << ". Message: "
+//				<< e.what();
+//
+//		}
+//
+//	}
+//
+//private:
+//	asio::io_service& m_ios;
+//	asio::ip::tcp::acceptor m_acceptor;
+//
+//	asio::ssl::context m_tls_context;
+//
+//public:
+//	asio::ssl::stream<asio::ip::tcp::socket> *m_tls_stream;
+//
+//};
+//
+//class Server {
+//public:
+//	Server() : m_stop(false) {}
+//
+//	void start(unsigned short port_num) {
+//		run(port_num);
+//
+//		/*m_thread.reset(new std::thread([this, port_num]() {
+//			run(port_num);
+//		}));*/
+//	}
+//
+//	void stop() {
+//		m_stop.store(true);
+//		//m_thread->join();
+//	}
+//
+//	asio::ssl::stream<asio::ip::tcp::socket>* get_tls_stream()
+//	{
+//		return acc->m_tls_stream;
+//	}
+//
+//	~Server()
+//	{
+//		delete acc;
+//	}
+//
+//private:
+//	void run(unsigned short port_num) {
+//		acc = new Acceptor(m_ios, port_num);
+//
+//		acc->accept(&bConnectState);
+//
+//
+//		/*while (!m_stop.load()) {
+//			acc->accept();
+//		}*/
+//	}
+//
+//
+//	//std::unique_ptr<std::thread> m_thread;
+//	std::atomic<bool> m_stop;
+//	asio::io_service m_ios;
+//
+//public:
+//
+//	bool bConnectState = false;
+//
+//	Acceptor *acc;
+//
+//};
+//
+//int main()
+//{
+//	unsigned short port_num = 9090;
+//
+//	int i = 0;
+//
+//	try {
+//		Server srv;
+//		srv.start(port_num);
+//
+//		while (!srv.bConnectState)
+//		{
+//			std::cout << "Acceptor wait" << std::endl;
+//			std::this_thread::sleep_for(std::chrono::seconds(3));
+//		}
+//
+//		asio::ssl::stream<asio::ip::tcp::socket> *test_tls_stream = srv.get_tls_stream();
+//
+//		while (true)
+//		{
+//			try {
+//
+//				if (i > 10)
+//				{
+//					break;
+//				}
+//
+//				std::string response = "Write Response" + std::to_string(i) + "\n";
+//				asio::write(*test_tls_stream, asio::buffer(response));
+//
+//				std::cout << "Send Data to client" << std::endl;
+//				i++;
+//
+//				std::this_thread::sleep_for(std::chrono::seconds(3));
+//
+//			}
+//			catch (system::system_error &e) {
+//				std::cout << "Error occured! Error code = "
+//					<< e.code() << ". Message: "
+//					<< e.what();
+//			}
+//		}
+//
+//		srv.stop();
+//	}
+//	catch (system::system_error &e) {
+//		std::cout << "Error occured! Error code = "
+//			<< e.code() << ". Message: "
+//			<< e.what();
+//	}
+//
+//	return 0;
+//}
