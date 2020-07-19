bb881b7ad1ed647d61bf1b8872b4071d01f55351
idhpaul <idhpaul@naver.com>
2020-07-19 22:54:27 +0900

    seed module done

--- RnD_Cryptograph/SEED128/SEED_CBC_BASE64/main.cpp
+++ RnD_Cryptograph/SEED128/SEED_CBC_BASE64/main.cpp
@@ -15,7 +15,6 @@
 
 #include "b64.h"
 #include "seedcbc.h"
-#include <atlconv.h>
 
 #include <string>
 #include <iostream>
@@ -39,21 +38,27 @@ int main(){
 		return -1;
 	}
 
+	//for (int i = 0; i < 16; i++)
+	//{
+	//	key[i] = *(const_cast<char*>(str_seed_key.c_str() + i));
+	//	iv[i] = *(const_cast<char*>(str_seed_key.c_str() + i));
+	//}
+
 	/* 대칭키와 IV */
 	//unsigned char key[16] = { 0xED, 0x24, 0x01, 0xAD, 0x22, 0xFA, 0x25, 0x59, 0x91, 0xBA, 0xFD, 0xB0, 0x1F, 0xEF, 0xD6, 0x97 };
 	//unsigned char iv[16] = { 0x93, 0xEB, 0x14, 0x9F, 0x92, 0xC9, 0x90, 0x5B, 0xAE, 0x5C, 0xD3, 0x4D, 0xA0, 0x6C, 0x3C, 0x8E };
 	// chlguswls!@#giga
-	//unsigned char key[16] = { 0x63, 0x68, 0x6c, 0x67, 0x75, 0x73, 0x77, 0x6C, 0x73, 0x21, 0x40, 0x23, 0x67, 0x69, 0x67, 0x61 };
-	//unsigned char iv[16] = { 0x63, 0x68, 0x6c, 0x67, 0x75, 0x73, 0x77, 0x6C, 0x73, 0x21, 0x40, 0x23, 0x67, 0x69, 0x67, 0x61 };
+	unsigned char key[16] = { 0x63, 0x68, 0x6c, 0x67, 0x75, 0x73, 0x77, 0x6C, 0x73, 0x21, 0x40, 0x23, 0x67, 0x69, 0x67, 0x61 };
+	unsigned char iv[16] = { 0x63, 0x68, 0x6c, 0x67, 0x75, 0x73, 0x77, 0x6C, 0x73, 0x21, 0x40, 0x23, 0x67, 0x69, 0x67, 0x61 };
 
-	unsigned char key[16] = {0,};
-	unsigned char iv[16] = {0,};
+	//unsigned char key[16] = { 'A','2','3','1','9','3','A','8','0','F','1','5','M','S','0','7' };
+	//unsigned char iv[16] = { 'D','C','8','8','E','0','F','5','1','B','6','0','0','0','1','6' };
 
-	for (int i = 0; i < 16; i++)
-	{
-		key[i] = *(const_cast<char*>(str_seed_key.c_str() + i));
-		iv[i] = *(const_cast<char*>(str_seed_key.c_str() + i));
-	}
+	//for (int i = 0; i < 16; i++)
+	//{
+	//	key[i] = *(const_cast<char*>(str_seed_key.c_str() + i));
+	//	iv[i] = *(const_cast<char*>(str_seed_key.c_str() + i));
+	//}
 
 	
 	/* 입출력 버퍼 */
@@ -108,6 +113,86 @@ int main(){
 	printf("연결성공\n");
 
 #endif
+	std::locale::global(std::locale(""));
+	std::wcout.imbue(std::locale());
+
+	const unsigned char* test_str = (const unsigned char*)"한글123abc";
+	int test_size = 0;
+	unsigned char* test_out_str;
+	test_out_str = __base64_encode(test_str, strlen((char*)test_str), &test_size);
+
+	std::cout << test_out_str << std::endl;
+
+	unsigned char* test_out_decode_str;
+	int test_out_size = strlen((char*)test_out_str);
+
+	test_out_decode_str = __base64_decode(test_out_str, strlen((char*)test_out_str), &test_out_size);
+	std::cout << test_out_decode_str << std::endl;
+
+	std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
+	std::cout << "암호화 테스트" << std::endl;
+
+	char* plainStr = NULL;
+	int plainStr_Size = 0;
+
+	unsigned char seed_encoded_Str[BUF_SIZE + 16] = "\0";
+	int encoded_Cyper_Size = 0;
+
+	unsigned char* base64_encoded_Str = NULL;
+	int base64_encoded_Size = 0;
+
+	plainStr = (char*)"{\r\n  \"set-tx-configuration\": {\r\n    \"tx-mac\": \"00:67:a8:2d:84:7e\",\r\n    \"tx-ip\": \"232.123.132.154\",\r\n    \"rx-said\": \"PC000001\",\r\n    \"rx-client-id\": \"00:52:a8:2d:84:4e\",\r\n    \"rx-cust-id\":\"\",\r\n    \"auth-method\":\"auth-pw\",\r\n    \"port-1\": \"10001\",\r\n    \"port-2\": \"10002\",\r\n    \"port-3\": \"10003\",\r\n    \"port-4\": \"10004\",\r\n    \"port-5\": \"10005\",\r\n    \"zone-id\":\"ZONE0001\",\r\n    \"option\":\" \"\r\n  }\r\n}\r\n";
+	plainStr_Size = strlen((char*)plainStr);
+
+	// SEED Encode
+	encoded_Cyper_Size = KISA_SEED_CBC_ENCRYPT(key, iv, (unsigned char*)plainStr, plainStr_Size, seed_encoded_Str);
+
+	// Base64 Encode
+	//! You must "free()" output str after process done
+	base64_encoded_Str = __base64_encode((unsigned char*)seed_encoded_Str, encoded_Cyper_Size, &base64_encoded_Size);
+
+
+	std::cout << "[Encode] PlainStr : " << base64_encoded_Str << std::endl;
+	std::cout << "[Encode] PlainStr size : " << base64_encoded_Size << std::endl;
+	std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
+
+
+
+	std::cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << std::endl;
+	std::cout << "복호화 테스트" << std::endl;
+
+	unsigned char* base64_CyperStr = NULL;
+	int base64_CyperStr_Size = 0;
+	
+	unsigned char* seed_CyperStr = NULL;
+
+	unsigned char decoded_PlainStr[BUF_SIZE] = "\0";
+	int decoded_PlainStr_Size = 0;
+
+
+	// Init base64 cyper String
+	base64_CyperStr = (unsigned char*)"pBvdqQGRAsecaAT4V2kMvw==";
+	base64_CyperStr_Size = strlen((char*)base64_CyperStr); // Not include \0
+	//base64_CyperStr = base64_encoded_Str;
+	//base64_CyperStr_Size = strlen((char*)base64_CyperStr); // Not include \0
+
+	// Base64 Decode
+	//! You must "free()" output str after process done
+	seed_CyperStr = __base64_decode(base64_CyperStr, strlen((char*)base64_CyperStr), &base64_CyperStr_Size);
+
+	std::cout << "base64 decode after size : " << base64_CyperStr_Size << std::endl;
+
+	// SEED Decode
+	decoded_PlainStr_Size = KISA_SEED_CBC_DECRYPT(key, iv, seed_CyperStr, base64_CyperStr_Size, decoded_PlainStr);
+
+	std::cout << "[Decode] PlainStr : " << decoded_PlainStr << std::endl;
+	std::cout << "[Decode] PlainStr size : " << decoded_PlainStr_Size << std::endl;
+	std::cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << std::endl;
+
+	free(base64_encoded_Str);
+	free(seed_CyperStr);
+
+
 
 	while(1){
 		memset(&recv_text, 0, sizeof(recv_text));
@@ -116,8 +201,8 @@ int main(){
 		//printf("SEED 암호화할 데이터를 입력하세요: ");
 		//fgets((char *)plaintext, sizeof(plaintext), stdin); /* 암호화할 평문 사용자입력 */
 
-		char* plaintext = (char*)"{\r\n  \"hello\": {\r\n    \"client-info\": {\r\n      \"said\":\"MSLM0001\",\r\n      \"client-id\":\"MSLM0001\",\r\n      \"device-name\": \"SW RX\",\r\n      \"device-version\": \"1.0.0\",\r\n      \"platform\": \"syncpc-client-OS\",\r\n      \"locale\": \"ko_KO\",\r\n      \"hostname\": \"syncpc.com\",\r\n      \"serial-number\": \"123456\",\r\n      \"mac\": \"00:67:a8:2d:84:7e\"\r\n     },\r\n    \"CBS-info\": {\r\n      \"device-name\": \"kt Connection Broker\",\r\n      \"device-version\": \"1.0.0.12345\",\r\n      \"platform\": \"Linux Ubuntu 10.4 64-bit\",\r\n      \"ip-address\": \"10.0.136.93\",\r\n      \"hostname\": \"CBS.kt.com\"\r\n     },\r\n    \"negotiable capability\": {\r\n      \"capability\": [\r\n        \"NEGO_OAUTH\",\r\n        \"NEGO_PASSWORD\"\r\n      ]\r\n    },\r\n    \"client-RMS-options\": {\r\n      \"option\": {\r\n        \"name\": \"option-name\",\r\n        \"value\": \"option-value\"\r\n      }\r\n    }\r\n  }\r\n}";
-
+		//char* plaintext = (char*)"{\r\n  \"hello\": {\r\n    \"client-info\": {\r\n      \"said\":\"MSLM0001\",\r\n      \"client-id\":\"MSLM0001\",\r\n      \"device-name\": \"SW RX\",\r\n      \"device-version\": \"1.0.0\",\r\n      \"platform\": \"syncpc-client-OS\",\r\n      \"locale\": \"ko_KO\",\r\n      \"hostname\": \"syncpc.com\",\r\n      \"serial-number\": \"123456\",\r\n      \"mac\": \"00:67:a8:2d:84:7e\"\r\n     },\r\n    \"CBS-info\": {\r\n      \"device-name\": \"kt Connection Broker\",\r\n      \"device-version\": \"1.0.0.12345\",\r\n      \"platform\": \"Linux Ubuntu 10.4 64-bit\",\r\n      \"ip-address\": \"10.0.136.93\",\r\n      \"hostname\": \"CBS.kt.com\"\r\n     },\r\n    \"negotiable capability\": {\r\n      \"capability\": [\r\n        \"NEGO_OAUTH\",\r\n        \"NEGO_PASSWORD\"\r\n      ]\r\n    },\r\n    \"client-RMS-options\": {\r\n      \"option\": {\r\n        \"name\": \"option-name\",\r\n        \"value\": \"option-value\"\r\n      }\r\n    }\r\n  }\r\n}";
+		char* plaintext = (char*)"{\r\n  \"set-tx-configuration\": {\r\n    \"tx-mac\": \"00:67:a8:2d:84:7e\",\r\n    \"tx-ip\": \"232.123.132.154\",\r\n    \"rx-said\": \"PC000001\",\r\n    \"rx-client-id\": \"00:52:a8:2d:84:4e\",\r\n    \"rx-cust-id\":\"\",\r\n    \"auth-method\":\"auth-pw\",\r\n    \"port-1\": \"10001\",\r\n    \"port-2\": \"10002\",\r\n    \"port-3\": \"10003\",\r\n    \"port-4\": \"10004\",\r\n    \"port-5\": \"10005\",\r\n    \"zone-id\":\"ZONE0001\",\r\n    \"option\":\" \"\r\n  }\r\n}\r\n";
 		//std::string strtest = plaintext;
 		//ReplaceAll(strtest, std::string("\""), std::string("@"));
 		//std::cout << strtest << std::endl;
@@ -139,7 +224,7 @@ int main(){
 		str = __base64_encode((unsigned char *)ciphertext, after_encrypt_len, &size);
 
 		printf("base64 enc\n");
-		printf("%s\n", str);
+		//printf("%s\n", str);
 
 #if SOCKET_TEST_SIM
 		/* 서버로 암호화한 데이터 전송 */
@@ -154,7 +239,7 @@ int main(){
 		printf("서버로부터 받은 데이터: %s", recv_text);
 #endif
 
-		str = (unsigned char*)"Zt6UXN8g7RSAlyScgc4i1DsiFQMCGsHhJEN0zviwCyAbBgoMHRMsqHQLtnCboHpIe4D9hSiank2WegX6TERT6/KI7WDqUyvAjE+QtBqD1my9nhQHrxJIRz3ls6qqCR8eK4RaY5S9R06SpzohyaB9oPKDi9gOdb5Vl8pOIBhGRU+IFpNgApIxg+o+h82ZrNVZ25nvVbrG6KVHttotdEbWEZI++U/sZ103TlBttlz+WSe3VBM7xAn4dAKHgFTsA+Rz8QpmuqFWA1NrFnV7eJBWauJook127LT1qCu8bLQSIbSd7lLDKMDtH85Y04y41lxUoPf4hkflgDhMvE9Z9VPvVlTpcxwzBKPaYyFXkuB5NmZnAts2lumYP40UcVMP7hWfuSn/nSCoXNjGl0elPCcwgl8DW1Tzw4pLWclwLQNmJxhZ9jLRYNFV+xUHItC5By5qgi62co2SyJDmNe0wcVYSHRmT8rS1W6nc4QFgl8j0nGBaOtAPQJ3fmU0qZZwZ/hs1HmIGvkYjYNxAVa6/k0UGFzmqlc7EuWMVCJSahKuwZLdIjRzL2Wrdk7Cy3/Dau429tKwUOmvcE1IxTvdIwPihZGoFRhUo9AJzX8SmEoVx/9NNsGLexLRvS13+ugQVVJOM5cEWIG/0X3Bz0RuNpE+CH3YRNKGKVkSwMsfEpNQ0Hwd6nOMmEVIfTIu31VBSAiYLsl0ycDeZWCPDSNvsKwZZONySbFX2C90lGFKEhOqIQXaOVh0GgmKzVE//kwld6hIV36H6PRkTdtZ1F3Cs/p+wZQ==";
+		str = (unsigned char*)"7hnLAjhTj+zd+sjhAwN/5Q==";
 		size = strlen((char*)str);
 
 		/* 서버로부터 받은 암호화 데이터를 Base64 디코딩 */
@@ -210,17 +295,6 @@ void error_handling(char *msg){
 	exit(1);
 }
 
-void convert_string2wstring(const std::string& src, std::wstring& dest)
-{
-	USES_CONVERSION;
-	dest = A2W(src.c_str());
-}
-
-void convert_wstring2string(const std::wstring& src, std::string& dest)
-{
-	USES_CONVERSION;
-	dest = W2A(src.c_str());
-}
 
 std::string ReplaceAll(std::string& str, const std::string& from, const std::string& to) {
 	size_t start_pos = 0; //string처음부터 검사
